160f5480551958adece2f554710112bb4430592f
Nijat Muhtar <yasunaz1990@gmail.com>
2021-09-21 18:16:42 -0400

    initial project image

---
+++ src/lecture_0_helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package lecture_0_helloworld;
+
+// Class is used to contain and organize
+// related codes
+public class HelloWorld {
+    // Main method:
+    // program execution starts here
+    public static void main(String[] args) {
+        System.out.println("Hello World");       // prints out message to the screen
+    }
+}
