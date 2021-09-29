ad55310a4b762586d0ea59a80b213127772d1a30
Dustin Mahle <dmahle20401@gmail.com>
2021-09-29 19:27:43 -0400

    class practice

---
+++ src/lecture_0_helloworld/DustinGreeting.java
@@ -0,0 +1,41 @@
+package lecture_0_helloworld;
+
+public class DustinGreeting
+{
+    public static void main(String[] args)
+    {
+         System.out.println("Hello my name is Dustin"); //typing out to terminal a message
+         System.out.println(3.14);                      // typing out the value of pi constant
+         System.out.println(30);                       // typing out the age of my brother
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
